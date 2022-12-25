20762813b5ae14f514f147ed6228297a65da6ab6
jkkelley <jkkelley86@gmail.com>
2022-12-24 21:05:50 -0600

    Showing all dynamic data correctly

--- src/Components/Flairs.jsx
+++ src/Components/Flairs.jsx
@@ -2,30 +2,35 @@ import { Card, CardHeader, CardContent, Typography } from "@mui/material";
 import React, { useEffect, useState } from "react";
 
 function Flairs(flairs) {
-  const [flair, setFlair] = useState("");
-  const [stats, setStats] = useState([]);
+  const [flair, setFlair] = useState([]);
 
   useEffect(() => {
     if (flairs?.flairs) {
-      Object.entries(flairs.flairs).forEach((key) => {
-        setFlair(key[0]);
-        setStats(flairs.flairs[key[0]]);
-      });
+      setFlair(flairs.flairs);
     }
-  }, [flairs.flairs, stats]);
+  }, [flairs.flairs]);
 
+  let results = [];
+  flair.forEach((object, idx) => {
+    let quality = Object.keys(object);
+    quality.forEach((qual) => {
+    let description = object[qual]?.Description;
+      results.push(
+        <CardContent key={idx}>
+          <Typography>{qual}:</Typography>
+          <Typography>{description}</Typography>
+        </CardContent>
+      );
+    });
+  });
+  
   if (!flairs.flairs) return "";
 
   return (
     <>
       <Card>
-        <CardHeader title={flair} />
-        {Object.entries(stats).map((key, idx) => (
-          <CardContent key={idx}>
-            <Typography>{key[0]}:</Typography>
-            <Typography>{key[1]}</Typography>
-          </CardContent>
-        ))}
+        <CardHeader title={`Flairs:`} />
+        {results}
       </Card>
     </>
   );
