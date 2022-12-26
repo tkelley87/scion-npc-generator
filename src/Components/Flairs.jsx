974abe8bc507286ba126e1cb4e80ee74e1abe463
jkkelley <jkkelley86@gmail.com>
2022-12-26 12:27:31 -0600

    fix(main): Added dynamic naming

--- src/Components/Flairs.jsx
+++ src/Components/Flairs.jsx
@@ -2,19 +2,25 @@ import { Card, CardHeader, CardContent, Typography } from "@mui/material";
 import React, { useEffect, useState } from "react";
 
 function Flairs(flairs) {
+
   const [flair, setFlair] = useState([]);
+  const [stat, setStat] = useState("")
 
   useEffect(() => {
     if (flairs?.flairs) {
       setFlair(flairs.flairs);
+      let name =
+        Object.entries(flairs)[0][0].charAt(0).toUpperCase() +
+        Object.entries(flairs)[0][0].slice(1);
+      setStat(name)
     }
-  }, [flairs.flairs]);
+  }, [flairs.flairs, flairs]);
 
   let results = [];
   flair.forEach((object, idx) => {
     let quality = Object.keys(object);
     quality.forEach((qual) => {
-    let description = object[qual]?.Description;
+      let description = object[qual]?.Description;
       results.push(
         <CardContent key={idx}>
           <Typography>{qual}:</Typography>
@@ -23,13 +29,13 @@ function Flairs(flairs) {
       );
     });
   });
-  
+
   if (!flairs.flairs) return "";
 
   return (
     <>
       <Card>
-        <CardHeader title={`Flairs:`} />
+        <CardHeader title={`${stat}`} />
         {results}
       </Card>
     </>
