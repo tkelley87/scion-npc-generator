9f4b471de8c99dc83ff443bcac342b50234a63ff
jkkelley <jkkelley86@gmail.com>
2023-01-10 15:11:01 -0600

    added exact route

--- src/App.js
+++ src/App.js
@@ -55,6 +55,7 @@ function App() {
             <Route
               path="/"
               element={<Form insertedArticle={insertedArticle} />}
+              exact
             ></Route>
           </Routes>
         </BrowserRouter>
