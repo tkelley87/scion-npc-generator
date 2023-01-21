932df4d5e9b1875f6563e356791dbf0189f27b4a
jkkelley <jkkelley86@gmail.com>
2023-01-21 13:50:56 -0600

    fix

--- _infra/variables.tf
+++ _infra/variables.tf
@@ -30,9 +30,9 @@
 #   default     = ["10.0.0.0/20", "10.0.32.0/20", "10.0.64.0/20"]
 # }
 
-# variable "region" {
-#   default = "us-east-1"
-# }
+variable "region" {
+  default = "us-east-1"
+}
 
 # // INJECTED AT BUILD TIME VARIABLES
 
