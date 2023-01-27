4e646c58e0c4e3c9274f57ccb73e6a9cac5f6979
jkkelley <jkkelley86@gmail.com>
2023-01-27 16:00:25 -0600

    Added another AZ

--- _infra/variables.tf
+++ _infra/variables.tf
@@ -1,6 +1,6 @@
 variable "availability_zones" {
   description = "a comma-separated list of availability zones, defaults to all AZ of the region, if set to something other than the defaults, both private_subnets and public_subnets have to be defined as well"
-  default     = ["us-east-1a"] # ADD back in for HA => , "us-east-1b", "us-east-1c"
+  default     = ["us-east-1a", "us-east-1b"] # ADD back in for HA => , "us-east-1c"
 }
 
 variable "cidr" {
@@ -43,12 +43,12 @@ variable "name" {
 
 variable "public_subnets" {
   description = "List of CIDRs for public subnets in your VPC, must be set if the cidr variable is defined, needs to have as many elements as there are availability zones"
-  default     = ["10.0.16.0/20"] # ADD back in for HA => , "10.0.48.0/20", "10.0.80.0/20"
+  default     = ["10.0.16.0/20", "10.0.48.0/20"] # ADD back in for HA => , "10.0.80.0/20"
 }
 
 variable "private_subnets" {
   description = "List of CIDRs for private subnets in your VPC, must be set if the cidr variable is defined, needs to have as many elements as there are availability zones"
-  default     = ["10.0.0.0/20"] # ADD back in for HA => , "10.0.32.0/20", "10.0.64.0/20"
+  default     = ["10.0.0.0/20", "10.0.32.0/20"] # ADD back in for HA => , "10.0.64.0/20"
 }
 
 variable "region" {
