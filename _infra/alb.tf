a18805cf7f06fbc2d3afaacabf5bfa9c43bce1c3
jkkelley <jkkelley86@gmail.com>
2023-01-27 15:54:22 -0600

    Test

--- _infra/alb.tf
+++ _infra/alb.tf
@@ -2,7 +2,7 @@ module "alb" {
   source      = "./tf_aws_alb"
   name        = var.name
   ecs_sg_id   = module.ecs.ecs_sg_id
-  vpc_id      = module.vpc.id
+  vpc_id      = module.vpc.vpc_id
   subnets     = module.vpc.public_subnets
   environment = var.environment
   # alb_tls_cert_arn    = var.tsl_certificate_arn
