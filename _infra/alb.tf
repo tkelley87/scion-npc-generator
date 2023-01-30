7b8d1c88d9cca39ba4b66b210c8a49cccd943e60
jkkelley <jkkelley86@gmail.com>
2023-01-30 14:52:11 -0600

    Test

--- _infra/alb.tf
+++ _infra/alb.tf
@@ -1,7 +1,6 @@
 module "alb" {
   source            = "./tf_aws_alb"
   name              = var.name
-  ecs_sg_id         = module.ecs.ecs_sg_id
   vpc_id            = module.vpc.vpc_id
   subnets           = module.vpc.public_subnets
   environment       = var.environment
--- _infra/fe-react.tf
+++ _infra/fe-react.tf
@@ -5,6 +5,7 @@ module "fe-react" {
   # path_pattern = "/"
 
   ecs_cluster_id        = module.ecs.ecs_cluster_id
+  ecs_sg_id             = module.ecs.ecs_sg_id
   environment           = var.environment
   health_check_path     = var.health_check_path
   name                  = var.name
