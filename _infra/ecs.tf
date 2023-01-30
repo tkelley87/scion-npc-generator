729a4ae74630f1e44e939883eb07cededfbc760a
jkkelley <jkkelley86@gmail.com>
2023-01-30 14:38:17 -0600

    t

--- _infra/ecs.tf
+++ _infra/ecs.tf
@@ -3,12 +3,7 @@ module "ecs" {
   environment = var.environment
   name        = var.name
   vpc_id      = module.vpc.vpc_id
-  container_environment = [
-    { name = "LOG_LEVEL",
-    value = "DEBUG" },
-    { name = "PORT",
-    value = var.container_port }
-  ]
+
 }
 
 # module "ecs" {
