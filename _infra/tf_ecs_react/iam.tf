a327252a8aaae426c14544d0785674c742b1854b
jkkelley <jkkelley86@gmail.com>
2023-01-30 15:11:47 -0600

    Test

--- _infra/tf_ecs_react/iam.tf
+++ _infra/tf_ecs_react/iam.tf
@@ -18,6 +18,41 @@ resource "aws_iam_role" "ecs_task_role" {
 EOF
 }
 
+resource "aws_iam_policy" "dynamodb" {
+  name        = "${var.name}-task-policy-dynamodb"
+  description = "Policy that allows access to DynamoDB"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+      {
+          "Effect": "Allow",
+          "Action": [
+              "dynamodb:CreateTable",
+              "dynamodb:UpdateTimeToLive",
+              "dynamodb:PutItem",
+              "dynamodb:DescribeTable",
+              "dynamodb:ListTables",
+              "dynamodb:DeleteItem",
+              "dynamodb:GetItem",
+              "dynamodb:Scan",
+              "dynamodb:Query",
+              "dynamodb:UpdateItem",
+              "dynamodb:UpdateTable"
+          ],
+          "Resource": "*"
+      }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "ecs-task-role-policy-attachment" {
+  role       = aws_iam_role.ecs_task_role.name
+  policy_arn = aws_iam_policy.dynamodb.arn
+}
+
 resource "aws_iam_role" "ecs_task_execution_role" {
   name = "${var.name}-ecsTaskExecutionRole"
 
