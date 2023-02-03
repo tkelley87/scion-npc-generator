ec633cd4f2b5cb59bfb91f16cc124b19501ed651
jkkelley <jkkelley86@gmail.com>
2023-02-02 19:13:50 -0600

    Gunicorn logging

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,12 @@
+import logging
 from src import create_app
 
 app = create_app()
 
 if __name__ == "__main__":
     app.run()
+
+else:
+    gunicorn_logger = logging.getLogger('gunicorn.error')
+    app.logger.handlers = gunicorn_logger.handlers
+    app.logger.setLevel(gunicorn_logger.level)
