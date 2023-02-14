18bda5be91588cdaa617f4b2d20aa4f4dd1789aa
jkkelley <jkkelley86@gmail.com>
2023-02-14 10:27:08 -0600

    This may need to change.

--- deployment/prod.docker_entrypoint.sh
+++ deployment/prod.docker_entrypoint.sh
@@ -3,12 +3,12 @@
 echo "Sleeping for 0.5 seconds..."
 sleep 0.5
 
-/bin/sed -i "s/{{AWS_ACCESS_KEY_ID}}/$AWS_ACCESS_KEY_ID/g" .env.prod
-/bin/sed -i "s/{{AWS_SECRET_ACCESS_KEY}}/$AWS_SECRET_ACCESS_KEY/g" .env.prod
-/bin/sed -i "s/{{REGION_NAME}}/$REGION_NAME/g" .env.prod
-/bin/sed -i "s|{{DYNAMODB_URI}}|$DYNAMODB_URI|g" .env.prod
-/bin/sed -i "s/{{CONFIG_TYPE}}/$CONFIG_TYPE/g" .env.prod
-/bin/sed -i "s|{{FLASK_APP}}|$FLASK_APP|g" .env.prod
+# /bin/sed -i "s/{{AWS_ACCESS_KEY_ID}}/$AWS_ACCESS_KEY_ID/g" .env
+# /bin/sed -i "s/{{AWS_SECRET_ACCESS_KEY}}/$AWS_SECRET_ACCESS_KEY/g" .env
+/bin/sed -i "s/{{REGION_NAME}}/$REGION_NAME/g" .env
+# /bin/sed -i "s|{{DYNAMODB_URI}}|$DYNAMODB_URI|g" .env
+/bin/sed -i "s/{{CONFIG_TYPE}}/$CONFIG_TYPE/g" .env
+/bin/sed -i "s|{{FLASK_APP}}|$FLASK_APP|g" .env
 
 echo "Sleeping for 0.5 seconds..."
 sleep 0.5
