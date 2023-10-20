26d9c05c693d88d8169fc1c898c53b919afa590f
jkkelley <jkkelley86@gmail.com>
2023-10-20 18:43:08 -0500

    * Added testing character

---
+++ src/testing/char.js
@@ -0,0 +1,126 @@
+const character_test = {
+  "Apart of Cult?": "Historian",
+  "Attitude towards player": "2",
+  Drawbacks: [
+    {
+      Vulnerability: {
+        Description:
+          "Choose a source of damage \u2014 for example, fire, salt, silver, etc. When the Antagonist takes any damage from this source, mark a second Health box as well.  If attack is a critical strike, mark four Health boxes instead of two.",
+      },
+    },
+  ],
+  Drive: "Realizing a dream",
+  Flairs: [
+    {
+      "Molon Labe": {
+        Action: "Reflexive",
+        Cooldown: "End of Scene",
+        Cost: "None or 1 Tension",
+        Description:
+          "The Antagonist gives a defiant sign or speech, which need not be complex \u2014 a few words will do. All allied characters receive +1 Health, +2 Defense, and a +2 Enhancement to their Primary combat pool. The nor- mal duration for this Flair is a single round \u2014 however, for every character Taken Out while under its effects, the duration is extended for another round. When the effect lapses, all benefits, including extra Health, fade. When marked Health boxes fade, do not shift the damage to other Health boxes. For 1 Tension, extend the effects of this Flair for an additional round.",
+        Duration: "Varies",
+        Range: "Medium",
+        Subject: "All allied characters",
+      },
+    },
+    {
+      "Divine Command": {
+        Action: "Simple",
+        Clash: "Presence + Legend vs. Desperation",
+        Cooldown:
+          "The scene ends; or targets released from Divine Command\u2019s effects attack the Antagonist",
+        Cost: "None",
+        Description:
+          "The Antagonist is charged by divine authority, and can command mortals and creatures of a similar tier. Divine Command can target individuals or groups. Targets are compelled to carry out the commands of the Antagonist unless the effect is successfully Clashed, which can take place at any time Divine Command is active. This requires an impassioned plea, forceful declaration, or similar action on the part of a Demigod.",
+        Duration: "One Scene",
+      },
+    },
+    {
+      Inspiration: {
+        Action: "Reflexive",
+        Cooldown: "End of scene",
+        Cost: "None",
+        Description:
+          "The Antagonist\u2019s presence lends strength to her allies. When invoked, all allied characters within near range temporarily remove one Injury Complication, or if they do not currently have one, gain a blanket +1 Enhancement to all actions. The Injury Complications return and the Enhancement fades when the Flair ends, when allies subject to it move out of range of the Antagonist, or when the Antagonist with this Flair is Taken Out. As Antagonists do not use Injury Complications, Antagonists affected by this Flair gain +1 Health, or +1 Enhancement if undamaged.",
+        Duration: "One scene",
+        Range: "Medium",
+        Subject: "All Allies",
+      },
+    },
+  ],
+  Gender: "Male",
+  Human: "yes",
+  Name: "Aarav",
+  Pantheon: "aesir",
+  Qualities: [
+    {
+      "Baleful Touch": {
+        Description:
+          "The Antagonist seethes with some kind of energy or substance inimical to life \u2014 anything from fire to acid to the touch of death itself. Any attack made by this Antagonist fills in an additional Health box, unless that Health box would inflict an Injury Complication greater than the one marked normally. For example, if a character only had one Bruised and one Injured Condition remaining, she would only mark Bruised. For 1 Tension, the effect lingers \u2014 the target\u2019s clothes catch on fire, the acid clings and keeps eating away, etc. If the target does not take an action on her next turn to ameliorate the effects (which can be Mixed), she will take one additional damage.",
+      },
+    },
+    {
+      "Mystic Arsenal": {
+        Description:
+          "The Antagonist is experienced with mythic forces, wielding them like the finest and most sophisticated of weapons. She likely possesses a Relic \u2014 if not, she has some other form of signature weapon or item that greatly empowers her. Any actions undertaken with this item receive a +2 Enhancement, and if the Storyguide spends 1 Tension, she may raise that to +4 for a single action. Choose a Purview \u2014 when wielding the item, she may channel it to create marvels at the cost of 1 Tension. Play fair \u2014 if the player characters defeat the Antagonist and take her stuff, they get a totally cool Relic or other mystical object. No takebacks. Of course, that doesn\u2019t mean the Antagonist\u2019s friends won\u2019t try to get it back! Work with the players to design an appropriate Relic using the rules provided for player characters.",
+      },
+    },
+    {
+      "Immortal-ish": {
+        Description:
+          "The Antagonist can be killed, but will inevitably return to life, whether by reanimating their body, reincarnating, or whatever method is appropriate. Choose a source of damage (fire, arrows, drowning, etc.) that can permanently kill the Antagonist, preferably something thematic such as antivenom for a venomous or otherwise toxic Antagonist.\nThis Quality does not apply if the Antagonist was slain by a being of greater Tier, or if the Antagonist has completed the Descent Deed of Apotheosis.",
+      },
+    },
+  ],
+  Relic: {
+    Prosperity: {
+      "All that Glitters": {
+        Action: "Simple",
+        Description:
+          "going unnoticed. You can use this Boon on either a luxury good that you\u2019ve bought this session, like an elegant necklace or brand-new SUV, or an object or locale that you have either concretely or symbolically dedicated to a group of people, like a shrine built for your cult or a shopping mall you\u2019ve cut the ribbon to. The blessed object or location catches the attention of anyone who sees it, and provides Enhancement 2 to influence that plays off a character\u2019s feelings towards it. You could use your shiny new jewelry to seduce a handsome-but-penniless bachelor, or give it away to an enemy, and then convince a crew of notorious thieves to rob their house once they\u2019ve seen the goods.",
+        Duration: "One Session",
+        Range: "Close",
+        Subject: "One Object or Location",
+      },
+      "Blessed Wealth": {
+        Action: "Simple",
+        Description:
+          "You call forth wealth, conjuring up roughly $10,000 worth of riches (though you don\u2019t need to bother keeping track of precise sums). This wealth takes the form of your choosing: a fresh wad of $100 bills in your wallet, coins of precious metal minted with your divine parent\u2019s visage, a discreet deposit into your online bank account, or similar. Regardless of the form it takes, money conjured with this Boon wants to be spent \u2014 any that remains in your possession once the session ends vanishes. The money retains your blessing once it has been given away, giving a mortal Enhancement 3 on a single roll to run a business successfully or maintain a comfortable home and lifestyle (above and beyond the usual benefits of having money to spend). If it becomes divided among multiple characters, only the first one to roll gets the Enhancement.",
+        Duration: "Instant",
+      },
+      "Divine Providence": {
+        Action: "Simple",
+        Description:
+          "You give your blessing to a specific group of mortals that belong to an organization or live in the same place, such as the employees of a business, the members of your cult, or the populace of a city. Your benevolence wards away economic misfortune, negating any mundane Conditions that arise from poverty or stabilizing larger-scale economies. A blessing on a city\u2019s homeless population ensures they will fortuitously find enough resources to maintain their basic needs of sustenance and accommodation, while your favored city will weather recessions or other misfortune slightly better than others in the region. If you have a relationship to the group or city that benefits from this that is represented by a Path, you can evoke that Path an additional time each session.",
+        Duration: "Indefinite",
+        Range: "Long",
+        Subject: "One Group or City",
+      },
+      "Innate Power": [
+        "Whenever you exploit your wealth or financial status to influence someone, treat their Attitude towards you as one point higher. This does not stack with other magical Attitude bonuses.",
+      ],
+    },
+  },
+  Stats: {
+    Defense: "5",
+    "Desperation Pool": "6",
+    Drawbacks: "1",
+    Extras:
+      "Avatar antagonists have a variety of Qualities and Flairs that replicate certain Boons, and have birthrights, and anything else to feel Godlike",
+    Flairs: "3",
+    Health: "6",
+    Initiative: "5",
+    "Primary Pool": "10",
+    Qualities: "3",
+    "Secondary Pool": "8",
+  },
+  Traits: [
+    "This NPC is constantly biting their nails out of nervousness.",
+    "This NPC always has some red stain on their hands. Surely its just tomato sauce... right?",
+    "This NPC has heterochromia (two different color eyes).",
+  ],
+  Vulnerability: "Necrotic",
+  id: "a0fc548b5515477ab205d2cb0703ea8a",
+};
+
+export default character_test;
