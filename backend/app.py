d280ebb11ba8850e856a4bf70da839727053422b
jkkelley <jkkelley86@gmail.com>
2023-10-21 10:58:11 -0500

    * Added section for local docker development logging WIP.

--- backend/app.py
+++ backend/app.py
@@ -10,3 +10,8 @@ else:
     gunicorn_logger = logging.getLogger('gunicorn.error')
     app.logger.handlers = gunicorn_logger.handlers
     app.logger.setLevel(gunicorn_logger.level)
+    # # Set up logging, Comment above out and un-comment below for local development.
+    # handler = logging.StreamHandler()
+    # handler.setLevel(logging.INFO)
+    # app.logger.addHandler(handler)
+    # app.logger.setLevel(logging.INFO)
